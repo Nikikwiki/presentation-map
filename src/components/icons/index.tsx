921ec8bd1996c0ebc887500614d1caa06529861d
Nikita <nikikwiki@mail.ru>
2022-03-04 02:21:25 +0300

    iteraction

--- src/components/icons/index.tsx
+++
@@ -1,7 +0,0 @@
-import CloseIcon from 'static/icons/icon-close.svg';
-
-import { createSvgIcon } from './create-svg-icon';
-
-export namespace Icons {
-    export const IconClose = createSvgIcon(CloseIcon);
-}
